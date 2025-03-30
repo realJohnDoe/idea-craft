38b45a6787c5cc3bcb936d9498fca5c1e2365248
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-03-30 17:44:03 +0200

    Fix layout and adapt import of task content

--- src/components/content-item/ContentBody.tsx
+++ src/components/content-item/ContentBody.tsx
@@ -77,8 +77,10 @@ const ContentBody: React.FC<ContentBodyProps> = ({
             className=""
           />
         </div>
-        <div className="flex flex-col gap-2 px-2 mt-1 mb-2">
-          {hasTaskAttributes(contentToItem(item)) && (
+        {(hasTaskAttributes(contentToItem(item)) ||
+          hasEventAttributes(contentToItem(item)) ||
+          hasMailAttributes(contentToItem(item))) && (
+          <div className="flex flex-col gap-2 px-2 mt-1 mb-2">
             <div className="flex items-center px-1">
               <IdeaCraftCheckbox
                 checked={item.taskDone}
@@ -95,30 +97,10 @@ const ContentBody: React.FC<ContentBodyProps> = ({
                 Mark as done
               </label>
             </div>
-          )}
-          {hasEventAttributes(contentToItem(item)) && (
-            <div className="px-1 text-sm text-event">
-              <div className="flex items-center">
-                <svg
-                  className="mr-1 w-4 h-4 text-event"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
-                  <line x1="16" x2="16" y1="2" y2="6" />
-                  <line x1="8" x2="8" y1="2" y2="6" />
-                  <line x1="3" x2="21" y1="10" y2="10" />
-                </svg>
-                <span>{format(item.eventDate, "PPPP")}</span>
-              </div>
 
-              {item.eventLocation && (
-                <div className="flex items-center mt-1">
+            {hasEventAttributes(contentToItem(item)) && (
+              <div className="px-1 text-sm text-event">
+                <div className="flex items-center">
                   <svg
                     className="mr-1 w-4 h-4 text-event"
                     xmlns="http://www.w3.org/2000/svg"
@@ -129,32 +111,53 @@ const ContentBody: React.FC<ContentBodyProps> = ({
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   >
-                    <path d="M12 21c-4.97-5.46-8-9.25-8-12a8 8 0 0116 0c0 2.75-3.03 6.54-8 12z"></path>
-                    <circle cx="12" cy="9" r="3"></circle>
+                    <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
+                    <line x1="16" x2="16" y1="2" y2="6" />
+                    <line x1="8" x2="8" y1="2" y2="6" />
+                    <line x1="3" x2="21" y1="10" y2="10" />
                   </svg>
-                  <span>{item.eventLocation}</span>
+                  <span>{format(item.eventDate, "PPPP")}</span>
                 </div>
-              )}
-            </div>
-          )}
 
-          {hasMailAttributes(contentToItem(item)) && (
-            <div className="px-1 text-sm text-mail">
-              {item.mailFrom && (
-                <div>
-                  <span className="font-medium">From:</span> {item.mailFrom}
-                </div>
-              )}
+                {item.eventLocation && (
+                  <div className="flex items-center mt-1">
+                    <svg
+                      className="mr-1 w-4 h-4 text-event"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M12 21c-4.97-5.46-8-9.25-8-12a8 8 0 0116 0c0 2.75-3.03 6.54-8 12z"></path>
+                      <circle cx="12" cy="9" r="3"></circle>
+                    </svg>
+                    <span>{item.eventLocation}</span>
+                  </div>
+                )}
+              </div>
+            )}
 
-              {item.mailTo && item.mailTo.length > 0 && (
-                <div>
-                  <span className="font-medium">To:</span>{" "}
-                  {item.mailTo.join(", ")}
-                </div>
-              )}
-            </div>
-          )}
-        </div>
+            {hasMailAttributes(contentToItem(item)) && (
+              <div className="px-1 text-sm text-mail">
+                {item.mailFrom && (
+                  <div>
+                    <span className="font-medium">From:</span> {item.mailFrom}
+                  </div>
+                )}
+
+                {item.mailTo && item.mailTo.length > 0 && (
+                  <div>
+                    <span className="font-medium">To:</span>{" "}
+                    {item.mailTo.join(", ")}
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        )}
       </Card>
 
       {processedContent && (
