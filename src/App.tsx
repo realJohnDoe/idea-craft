ba28000ad527aff2ae796883bf5b71aa66f805c8
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-04-12 22:03:33 +0200

    Add router base-name

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -14,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/idea-craft/">
         <Routes>
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="/" element={<Index />}>
