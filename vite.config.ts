e26e759db72df6dea3642d05fd1b44b9a91e6bf7
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-04-12 21:58:39 +0200

    Try with base

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
-
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  base: "/idea-craft/",
+  plugins: [react()].filter(Boolean),
   server: {
     port: 8080,
     host: "::",
     allowedHosts: [
       // Allow all hosts with a subdomain ending with lovableproject.com
-      'fef6ecf6-ba69-4bce-9ac5-d13267729f85.lovableproject.com',
-      '*.lovableproject.com'
+      "fef6ecf6-ba69-4bce-9ac5-d13267729f85.lovableproject.com",
+      "*.lovableproject.com",
     ],
   },
   resolve: {
